e90e243059b13b4927fddf6a7e74ef3ba27839e8
Tran My Hanh <tmyhanh870@gmail.com>
2025-10-28 02:14:27 +0700

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// --- Login
+import Login from "./pages/auth/Login.jsx";
+
+// --- Worker (Công nhân)
+import CongNhanLayout from "./layouts/CongNhan.jsx";
+import WorkerHome from "./pages/worker/Home.jsx";
+import WorkerShifts from "./pages/worker/Shifts.jsx";
+import WorkerSalary from "./pages/worker/Salary.jsx";
+
+// --- Director (Ban giám đốc)
+import BanGiamDocLayout from "./layouts/BanGiamDocLayout.jsx";
+import DirectorHome from "./pages/director/Home.jsx";
+import ApprovePlan from "./pages/director/ApprovePlan.jsx";
+import ApproveOrders from "./pages/director/ApproveOrders.jsx";
+import Reports from "./pages/director/Reports.jsx";
+
+// --- QC routes
 import QCRoute from "./routes/QCRoute";
 import KhoNVLRoute from "./routes/KhoNVLRoute";
 
 
-function App() {
-  return (
-    <Routes>
-      {/* Route QC */}
-      <Route path="/qc/*" element={<QCRoute />} />
+// --- Protected Route Component
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
-      {/* Route Kho NVL */}
-      <Route path="/kho/*" element={<KhoNVLRoute />} />
-    </Routes>
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/qc/*" element={<QCRoute />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
